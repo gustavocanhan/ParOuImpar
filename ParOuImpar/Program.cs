255a30c6d6feb28acbc3cabd680f447f04d85c7a
gustavocanhan <gustavocanhan@gmail.com>
2025-11-10 22:59:27 -0300

    Adicionar arquivos de projeto.

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParOuImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int entrada = -1;
+
+            while(entrada != 0)
+            {
+                Console.Write("Insira um número inteiro (0 para sair): ");
+
+                if (int.TryParse(Console.ReadLine(), out entrada))
+                {
+                    if(entrada % 2 == 0)
+                    {
+                        Console.WriteLine($"{entrada} - PAR.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{entrada} - ÍMPAR.\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, insira um número válido.\n");
+                    entrada = -1;
+                }
+            }
+
+            Console.WriteLine("Programa encerrado.");
+        }
+    }
+}
